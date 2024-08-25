cd2ae4f5085b56f9a78eabe71505421e0dd36e9b
mathpaul3 <mathpaul3@gmail.com>
2024-08-26 08:05:17 +0900

    feat: add pos tag

--- app/controller/nlp_controller.py
+++ app/controller/nlp_controller.py
@@ -7,7 +7,7 @@ router = APIRouter()
 
 
 @router.get("/tokenize")
-def tokenize_controller(sentence: str = "안녕하세요. 이것은 수어 번역기입니다.", db: Session = Depends(get_db)) -> list[dict[str, str]]:
+def tokenize_controller(sentence: str = "안녕하세요. 이것은 수어 번역기입니다.", db: Session = Depends(get_db)) -> dict[str, list[dict[str, str]]]:
     """
     문장을 토큰화하여 반환해주는 함수입니다.
     """
--- app/service/nlp.py
+++ app/service/nlp.py
@@ -6,17 +6,21 @@ from app.repository.nlp import get_url_by_word
 tokenizer = Okt()
 
 
-def get_nlp_result(sentence: str, db: Session) -> list[dict[str, str]]:
-    results = list()
+def get_nlp_result(sentence: str, db: Session) -> dict[str, list[dict[str, str]]]:
+    results: dict[str, list] = dict()
     words = tokenizer.pos(sentence, norm=True)
-    filter = ["Alpha", "Foreign", "Hashtag", "Josa", "ScreenName"]
+    filter = ["Alpha", "Foreign", "Hashtag",
+              "Josa", "Punctuation", "ScreenName"]
 
     PREFIX = "http://sldict.korean.go.kr/multimedia/multimedia_files/convert/"
     POSTFIX = "_700X466.webm"
     for (word, tag) in words:
-        url = "" if tag in filter else get_url_by_word(word, db)
-        results.append({
-            "word": word,
-            "url": f"{PREFIX}{url}{POSTFIX}" if url else ""
-        })
+        if tag not in filter:
+            if tag not in results:
+                results[tag] = []
+            url = get_url_by_word(word, db)
+            results[tag].append({
+                "word": word,
+                "url": f"{PREFIX}{url}{POSTFIX}" if url else ""
+            })
     return results
